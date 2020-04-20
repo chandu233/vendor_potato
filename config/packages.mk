a6ec7751f3b8c4110bf6e5fdb072e7352f3a96d7
ShubhamB <shubhamprince111@gmail.com>
2020-04-20 14:51:17 +0000

    prebuilt: Ship WallpaperPickerGoogle
    Change-Id: Ia52549e8e002f7c6cec8cfc1c837afaa3eb76375
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- config/packages.mk
+++ config/packages.mk
@@ -8,7 +8,8 @@ PRODUCT_PACKAGES += \
     StitchImage \
     ThemePicker \
     PixelThemesStub2019 \
-    MatchmakerPrebuilt
+    MatchmakerPrebuilt \
+    WallpaperPickerGoogle
 
 # ThemePicker
 PRODUCT_PACKAGES += \

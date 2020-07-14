7696566928090c90071811cf963bd44df1c0b6b4
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-07-14 07:07:09 +0200

    vendor: Port face unlock feature [2/4]
    Also add a flag to opt-out
    
    Change-Id: I9d4bb7174d28ca067665893a8096b3176a5aa7a6
    
    Signed-off-by: saikiran2001 <bjsaikiran@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -138,6 +138,16 @@ ifneq ($(TARGET_BUILD_VARIANT),user)
     SELINUX_IGNORE_NEVERALLOWS := true
 endif
 
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
 # Fonts
 PRODUCT_COPY_FILES += \
    vendor/potato/prebuilt/common/fonts/GoogleSans-Regular.ttf:system/fonts/GoogleSans-Regular.ttf \
